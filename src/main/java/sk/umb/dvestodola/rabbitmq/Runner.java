6826785508be52214c3d9d5456716c5e200d6028
avalastan-notebook <valastan1@gmail.com>
2023-11-13 18:34:10 +0100

    Added message queue response shaningans

--- src/main/java/sk/umb/dvestodola/rabbitmq/Runner.java
+++ src/main/java/sk/umb/dvestodola/rabbitmq/Runner.java
@@ -2,6 +2,7 @@ package sk.umb.dvestodola.rabbitmq;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -22,7 +23,9 @@ public class Runner implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     System.out.println("Sending message...");
-    rabbitTemplate.convertAndSend(RabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+    // Message bytes = (Message) rabbitTemplate.convertSendAndReceive(RabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+    Object bytes = rabbitTemplate.convertSendAndReceive(RabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+    System.out.println(bytes);
     // receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
   }
 
--- src/main/java/sk/umb/dvestodola/rabbitmq/services/Receiver.java
+++ src/main/java/sk/umb/dvestodola/rabbitmq/services/Receiver.java
@@ -2,6 +2,7 @@ package sk.umb.dvestodola.rabbitmq.services;
 
 import java.util.concurrent.CountDownLatch;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +10,12 @@ public class Receiver {
 
   private CountDownLatch latch = new CountDownLatch(1);
 
-  public void receiveMessage(String message) {
+  public byte[] receiveMessage(String message) {
     System.out.println("Received <" + message + ">");
+    byte[] bytes = new byte[message.length()];
+    System.arraycopy(message.getBytes(), 0, bytes, 0, message.length());
+    System.out.println("returning: " + bytes.toString());
+    return bytes;
     // latch.countDown();
   }
 
